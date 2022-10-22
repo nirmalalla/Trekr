f7fb4477f92a7b89a2401c5031e9ab51e4d46942
Nirmal Alla <nalla@quadyster.com>
2022-10-22 17:35:28 -0500

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Nirmal Alla on 10/22/22.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
